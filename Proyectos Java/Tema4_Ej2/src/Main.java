8b1fe046b42ae66f50bcd0645af06179d7d3b19d
Gonzalo Villalta <villalta_gonzalo@hotmail.com>
2022-09-21 16:55:45 -0500

    Ejercicio 2 del Tema 4

---
+++ Proyectos Java/Tema4_Ej2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    var numeroWhile = 0;
+
+    while (numeroWhile < 3) {
+        System.out.println(numeroWhile);
+        numeroWhile = numeroWhile + 1;
+    }
+
+    }
+
+}
