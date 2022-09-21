cd591ecb5b7884f3f7c1873bc1b8768723641914
Gonzalo Villalta <villalta_gonzalo@hotmail.com>
2022-09-21 17:29:52 -0500

    Ejercicio 4 del Tema 4

---
+++ Proyectos Java/Tema4_Ej4/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var numeroFor = 0;
+
+        for (numeroFor = 0; numeroFor <= 3; numeroFor = numeroFor + 1) {
+            System.out.println(numeroFor);
+
+        }
+    }
+}
