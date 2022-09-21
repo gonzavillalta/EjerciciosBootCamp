dfff72d4717e5146d20d62ce2d8849f110081acb
Gonzalo Villalta <villalta_gonzalo@hotmail.com>
2022-09-21 16:33:30 -0500

    Ejercicio 1 del tema 4

---
+++ Proyectos Java/Tema4_Ej1/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+       var numerolf = -3;
+
+        if (numerolf > 0) {
+            System.out.println("es_positivo");
+        } else if (numerolf < 0) {
+            System.out.println("es_negativo");
+        } else {
+            System.out.println("es_cero");
+    }
+    }
+
+
+}
